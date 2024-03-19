a309ab324cdbf3298aa4ff9b91555cb6a648c5ed
Darko-beep <darkofrederick98@outlook.com>
2024-03-19 16:42:16 +0000

    cart

--- single_product.php
+++ single_product.php
@@ -28,22 +28,6 @@ if(isset($_GET['product_id'])){
     <link rel="stylesheet"href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
     <link rel="stylesheet" href="boxicons.min.css">
     
-
-    <title>MushFarms Home</title>
-</head>
-<body>
-    
-  <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./assets/css/style.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet"href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
-    <link rel="stylesheet" href="boxicons.min.css">
-    
-
     <title>MushFarms Home</title>
 </head>
 <body>
@@ -91,6 +75,7 @@ if(isset($_GET['product_id'])){
 <section class="single-product my-5 pt-5">
     <div class="row mt-5">
       <?php while($row = $product->fetch_assoc()){ ?>
+
         <div class="col-lg-5 col-md-6 col-sm-12">
             <img class="img-fluid w-100 pb-1" src="/assets/images/<?php echo $row['product_image']; ?>" id="mainImg"/>
             <div class="small-img-group">
@@ -115,8 +100,16 @@ if(isset($_GET['product_id'])){
             <h6>Mushroom</h6>
             <h3 class="py-4"><?php echo $row['product_name']; ?></h3>
             <h2><?php echo $row['product_price']; ?></h2>
-            <input type="number" value="1">
-            <button class="buy-btn">Add to Cart</button>
+
+            <form method="POST" action="cart.php">
+              <input type="hidden" name="product_image" value="<?php echo $row['product_image']; ?>" />
+              <input type="hidden" name="product_name" value="<?php echo $row['product_name']; ?>" />
+              <input type="hidden" name="product_price" value="<?php echo $row['product_price']; ?>" />
+
+              <input type="number" name="product_quantity" value="1">
+              <button class="buy-btn" type="submit" name="add_to_cart" >Add to Cart</button>
+            </form>         
+            
             <h4 class="mt-5 mb-5">Product details</h4>
             <span><?php echo $row['product_description']; ?></span>
         </div>
