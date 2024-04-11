bb1fdead119e14fe2e2eeb114c7be0422ad26865
Darko-beep <darkofrederick98@outlook.com>
2024-04-11 09:23:59 +0000

    login

--- login.php
+++ login.php
@@ -1,3 +1,55 @@
+<?php
+
+session_start( );  // Initialize Session data
+include('server/connection.php');
+
+/*
+//PREVENT THE USER FROM REGISTERING ONCE LOGGED IN
+if(isset( $_SESSION['logged_in'])){
+  header("Location: account.php");
+  exit;
+}
+*/
+
+if(isset($_POST['login_btn'])) {
+
+  $email  = $_POST['email'];
+  $password = md5($_POST['password']);
+
+  $stmt = $conn->prepare("SELECT user_id,user_name,user_email,user_password FROM users WHERE user_email = ? AND user_password = ? LIMIT 1");
+
+  $stmt->bind_param("ss", $email, $password);
+
+  if( $stmt->execute()) {
+    $stmt->bind_result($user_id,$user_name,$user_email,$user_password);
+    $stmt->store_result();
+
+
+    if($stmt->num_rows( ) == 1) {
+
+     $stmt->fetch(); 
+
+     $_SESSION["user_id"] = $user_id;
+     $_SESSION["user_name"]= $user_name;
+     $_SESSION["user_email"] = $user_email;
+     $_SESSION["logged_in"] = true;
+
+     header('location: account.php?message=logged in successfully');
+
+    
+    }else{
+      header('location: login.php?error=could not verify your account');
+    }
+
+}else{
+  //error
+header('location: login.php?error=something went wrong');
+  }
+}
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -60,34 +112,28 @@
     </div>
 
     <div class="mx-auto container">
-        <form action="" id="login-form">
+        <form action="login.php" id="login-form"  method="POST" >
+          <p style="color: red;" class="text-center" ><?php if(isset($_GET['error'])){ echo $_GET['error']; } ?></p>
             <div class="form-group">
                 <label>Email</label>
-                <input type="text" class="form-control" id="login-email" placeholder="Email" required>
+                <input type="text" name="email"  class="form-control" id="login-email" placeholder="Email" required>
             </div>
 
             <div class="form-group">
                 <label>Password</label>
-                <input type="password" class="form-control" id="login-password" placeholder="password" required>
+                <input type="password" name="password" class="form-control" id="login-password" placeholder="password" required>
             </div>
 
             <div class="form-group">
-                <input type="submit" class="btn" id="login-btn" value="Login" >
+                <input type="submit" name="login_btn" class="btn" id="login-btn" value="Login" >
             </div>
 
             <div class="form-group">
-                <a id="register-url" class="btn" href="">Don't have an account? Register</a>
+                <a id="register-url" class="btn" href="register.php">Don't have an account? Register</a>
             </div>
         </form>
     </div>
 </section>
-
-
-
-
-
-
-
 
 <!--footer-->
 <footer class="mt-5 py-5">
@@ -160,11 +206,6 @@
     </div>
 </footer>
 <!--end of footer-->
-
-
-
-
-  
   
   <script src="https://unpkg.com/boxicons@2.1.3/dist/boxicons.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
