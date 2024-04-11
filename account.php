d3b16e281208cdffdab6f45be740585dcc2c2ee9
Darko-beep <darkofrederick98@outlook.com>
2024-04-11 09:46:01 +0000

    Account

--- account.php
+++ account.php
@@ -1,3 +1,14 @@
+<?php
+
+session_start();
+
+if(!isset($_SESSION["logged_in"])){
+header('location: login.php');
+exit;
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -13,21 +24,6 @@
 </head>
 <body>
     
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="./assets/css/style.css">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-        <link rel="stylesheet"href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
-        <link rel="stylesheet" href="boxicons.min.css">
-        
-    
-        <title>MushFarms Home</title>
-    </head>
-    <body>
-        
     <!--navbar beginning-->
     <nav class="navbar navbar-expand-lg navbar-light bg-white py-3 fixed-top">
       <div class="container-fluid">
@@ -75,9 +71,9 @@
             <h3 class="font-weight-bold">Account info</h3>
             <hr class="mx-auto">
             <div class="account-info">
-                <p>Name: <span>John Doe</span></p>
-                <p>Email: <span>john@email.com</span></p>
-                <p><a href="" id="order-btn">Your orders</a></p>
+                <p>Name: <span><?php if(isset($_SESSION['user_name'])){echo $_SESSION['user_name'];} ?></span></p>
+                <p>Email: <span><?php if(isset($_SESSION['user_email'])){echo $_SESSION['user_email'];} ?></span></p>
+                <p><a href="#orders" id="order-btn">Your orders</a></p>
                 <p><a href="" id="logout-btn">Logout</a></p>
             </div>  
         </div>
@@ -107,7 +103,7 @@
 
 
 <!--Orders-->
-<section class="orders container my-5 py-3">
+<section id="orders" class="orders container my-5 py-3">
     <div class="container mt-2">
         <h2 class="font-weight-bold text-center">Your Orders</h2>
         <hr class="mx-auto">
@@ -151,12 +147,6 @@
 
 
 </section>
-
-
-
-
-
-
 
 
 <!--footer-->
