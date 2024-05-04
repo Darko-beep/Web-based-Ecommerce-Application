f8f20d8d4c86183e1bb8a1347b16a5ec1da1c2fc
Darko-beep <darkofrederick98@outlook.com>
2024-05-04 10:12:28 +0000

    added php include to all the files for header and footer

---
+++ layouts/header.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
+    <link rel="stylesheet" href="boxicons.min.css">
+    <title>MushFarms Home</title>
+</head>
+<body>
+    
+<!--navbar beginning-->
+<nav class="navbar navbar-expand-lg navbar-light bg-white py-3 fixed-top">
+  <div class="container-fluid">
+    <!--a class="navbar-brand" href="#">Mushfarms</a> -->
+    <img class="logo" src="/assets/images/logo.jpg" alt="logo">
+    
+    <div class="collapse navbar-collapse nav-buttons" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        
+        <li class="nav-item">
+          <a class="nav-link" href="index.php">Home</a>
+        </li>
+        
+        <li class="nav-item">
+          <a class="nav-link" href="shop.html">Shop</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" href="#">Blog</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" href="contact.html">Contact Us</a>
+        </li>
+
+        <li class="nav-item">
+          <a href="cart.php"><box-icon name='cart-add'></box-icon></a>
+          <a href="account.html"><box-icon type='solid' name='user-account'></box-icon></a>
+        </li>
+
+        
+        
+      </ul>
+      
+    </div>
+  </div>
+</nav>
+<!--end of navbar-->
